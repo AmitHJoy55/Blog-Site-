cddad3a98249fdc8980a5085e69825d7c228401d
yasinbashar <107603927+yasinbashar@users.noreply.github.com>
2024-06-02 13:01:10 +0600

    Create blogSchema.js
    created blog schema

---
+++ Backend/models/blogSchema.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  mainImage: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  intro: {
+    type: String,
+    required: true,
+    minLength: [50, "Blog intro must contain at least 50 characters!"],
+  },
+  paraOneImage: {
+    public_id: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
+  },
+  paraOneDescription: {
+    type: String,
+  },
+  paraOneTitle: {
+    type: String,
+  },
+  
+  category: {
+    type: String,
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  adminName: {
+    type: String,
+    required: true,
+  },
+  adminAvatar: {
+    type: String,
+    required: true,
+  },
+  published: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export const Blog = mongoose.model("Blog", blogSchema);
