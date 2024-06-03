26058d1494ce53aad7b8bffda0769524baddbc02
yasinbashar <107603927+yasinbashar@users.noreply.github.com>
2024-06-04 01:37:34 +0600

    make logout, getMyProfile & get all admins api

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -2,12 +2,20 @@ import express from "express";
 import {
   register,
   login,
+  logout,
+  getAllAdmins,
+  getMyProfile,
 } from "../controllers/userController.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout", isAuthenticated, logout);
+router.get("/myprofile", isAuthenticated, getMyProfile);
+router.get("/admins", getAllAdmins);
+
 
 
 export default router;
