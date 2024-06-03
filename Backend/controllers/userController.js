e62cc2d6b33b161033bbc8e4f04fa24dbd5d7fcb
yasinbashar <107603927+yasinbashar@users.noreply.github.com>
2024-06-03 18:45:44 +0600

    added registration with jwt token

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,59 @@
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
+import ErrorHandler from "../middlewares/error.js";
+import { User } from "../models/userSchema.js";
+import { sendToken } from "../utils/jwtToken.js";
+import cloudinary from "cloudinary";
+
+export const register = catchAsyncErrors(async (req, res, next) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return next(new ErrorHandler("User Avatar Required!", 400));
+  }
+  const { avatar } = req.files;
+  const allowedFormats = ["image/png", "image/jpg", "image/jpeg", "image/webp"];
+  if (!allowedFormats.includes(avatar.mimetype)) {
+    return next(
+      new ErrorHandler(
+        "Invalid file type. Please provide your avatar in png, jpg, jpeg or webp format.",
+        400
+      )
+    );
+  }
+  const { name, email, password, registrationNo, role } = req.body;
+  if (
+    !name ||
+    !email ||
+    !password ||
+    !registrationNo ||
+    !role ||
+    !avatar
+  ) {
+    return next(new ErrorHandler("Please fill full details!", 400));
+  }
+  let user = await User.findOne({ email });
+  if (user) {
+    return next(new ErrorHandler("User already existes", 400));
+  }
+
+  const cloudinaryResponse = await cloudinary.uploader.upload(
+    avatar.tempFilePath
+  );
+  if (!cloudinaryResponse || cloudinaryResponse.error) {
+    console.error(
+      "Cloudinary error:",
+      cloudinaryResponse.error || "Unknown cloudinary error!"
+    );
+  }
+
+  user = await User.create({
+    name,
+    email,
+    password,
+    registrationNo,
+    role,
+    avatar: {
+      public_id: cloudinaryResponse.public_id,
+      url: cloudinaryResponse.secure_url,
+    },
+  });
+  sendToken(user, 200, "User registered successfully", res);
+});
