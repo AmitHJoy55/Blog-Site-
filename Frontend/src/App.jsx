65d8380b960e0575a99728889b4abed56298e9a2
yasinbashar <107603927+yasinbashar@users.noreply.github.com>
2024-06-07 23:47:37 +0600

    navbar added, routes created for pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../src/components/pages/Home";
+import About from "../src/components/pages/About";
+import Blogs from "../src/components/pages/Blogs";
+import Resource from "../src/components/pages/Resource";
+import SingleBlog from "../src/components/pages/SingleBlog";
+import Dashboard from "./components/pages/Dashboard";
+import Register from "./components/pages/Register";
+import Login from "./components/pages/Login";
+import AllAdmins from "./components/pages/AllAdmins";
+import UpdateBlog from "./components/pages/UpdateBlog";
+import Navbar from "./components/layout/Navbar";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blog/:id" element={<SingleBlog />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/admins" element={<AllAdmins />} />
+          <Route path="/resource" element={<Resource/>} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/blog/update/:id" element={<UpdateBlog />} />
+        </Routes>
+       
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Frontend/src/components/pages/AllAdmins.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllAdmins = () => {
+  return (
+    <div>
+      AllAdmins
+    </div>
+  )
+}
+
+export default AllAdmins
