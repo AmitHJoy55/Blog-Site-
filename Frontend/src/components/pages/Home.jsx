348d5013d23eec1262bdbb235fb6c66b94ea645a
Amit Hassan Joy <sarkeramit88@gmail.com>
2024-06-09 00:58:25 +0600

    Trending section added
    Trending section basically gonna contain the latest educational blogs.

--- Frontend/src/components/pages/Home.jsx
+++ Frontend/src/components/pages/Home.jsx
@@ -3,14 +3,12 @@ import {Context}  from '../../main' ;
 import HeroSection from '../miniComponents/HeroSection';
 import TrendingBlogs from '../miniComponents/TrendingBlogs';
 import LatestBlogs from '../miniComponents/LatestBlogs' ;
-import PopularAuthors from '../miniComponents/PopularAuthors' ;
 const Home = () => {
   const {mode} = useContext(Context);
   return <article className={mode === "dark" ? "dark-bg" : "light-bg" }>
     <HeroSection/>
     <TrendingBlogs/>
     <LatestBlogs/>
-    <PopularAuthors/>
     </article>
 }
 
